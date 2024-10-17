6cec39fa1f5889288853b79b9284f6aa9dd6f193
Pablo Guimarães <145077458+Pablo01Dev@users.noreply.github.com>
2024-10-17 10:15:52 -0300

    inicial

---
+++ potfolio/src/Data.jsx
@@ -0,0 +1,217 @@
+/* ======= Icones Tecnologias =======*/
+import { SiJavascript, SiTypescript, SiReact, SiAngular, SiBootstrap, SiStyledcomponents, SiNodedotjs, SiJquery } from 'react-icons/si';
+
+/*======== Import das imagens =======*/
+import imagem01 from '/public/game-master.jpg'
+import imagem02 from '/public/space-tourism.jpg'
+import imagem03 from '/public/poke-api.jpg'
+import imagem04 from '/public/hbo-clone.jpg'
+import imagem05 from '/public/calculadora-react.jpg'
+import imagem06 from '/public/disney+.jpg'
+
+
+export const links = [
+    {
+        name: 'Home',
+        path: 'home',
+    },
+    {
+        name: 'Services',
+        path: 'services',
+    },
+    {
+        name: 'Skills',
+        path: 'skills',
+    },
+    {
+        name: 'Portfolio',
+        path: 'portfolio',
+    },
+    {
+        name: 'Resume',
+        path: 'resume',
+    },
+    {
+        name: 'Contact',
+        path: 'contact',
+    },
+]
+
+export const services = [
+    {
+        id: 1,
+        name: 'Desenvolvimento Web',
+        title: 'Front-End',
+        description:'Desenvolvimento de sites profissionais, blogs, portfólios, landing pages e e-commerce.'
+    },
+    {
+        id: 2,
+        name: 'Designer',
+        title: 'UI Design',
+        description:'Desenho interfaces que sejam claras, objetivas e que possam ser intuitivas para o usuário. '
+    },
+]
+
+export const skills = [
+    {
+        id: 1,
+        name: 'JavaScript',
+        image: <SiJavascript id='js' />
+    },
+    {
+        id: 2,
+        name: 'TypeScript',
+        image: <SiTypescript id='ts' />
+    },
+    {
+        id: 3,
+        name: 'React',
+        image: <SiReact id='react' />
+    },
+    {
+        id: 4,
+        name: 'Angular',
+        image: <SiAngular id='angular' />
+    },
+    {
+        id: 5,
+        name: 'Bootstrap',
+        image: <SiBootstrap id='bootstrap' />
+    },
+    {
+        id: 6,
+        name: 'styled-components',
+        image: <SiStyledcomponents id='styled' />
+    },
+    {
+        id: 7,
+        name: 'jQuery',
+        image: <SiJquery id='jquery' />
+    },
+    {
+        id: 8,
+        name: 'Node js',
+        image: <SiNodedotjs id='node' />
+    },
+]
+
+export const projects = [
+    {
+        id: 1,
+        img: imagem01,
+        category: 'React',
+        title: 'App Game Master',
+        description:
+            'Este é um website que consome uma API de jogos, possui integração com Firebase para autenticação de usuário e armazenamento de dados com o firestore, construção com react e sass',
+        codigo: 'https://github.com/BrunoOliveira16/game-master',
+        demo: 'https://game-master-fawn.vercel.app/'
+    },
+    {
+        id: 2,
+        img: imagem02,
+        category: 'React',
+        title: 'SPA Turismo Espacial',
+        description:
+            'Este é um website de turismo espacial em React! Explore destinos incríveis, conheça a tripulação e as tecnologias usadas. Tudo com SPA, React-router e dados JSON.',
+        codigo: 'https://github.com/BrunoOliveira16/space-tourism-website',
+        demo: 'https://turismo-espacial.netlify.app/'
+    },
+    {
+        id: 3,
+        img: imagem03,
+        category: 'JavaScript',
+        title: 'Pokedex API',
+        description:
+            'O site utiliza HTML, CSS e JavaScript para criar uma interface dinâmica e responsiva. O site também usa conceitos de API, fetch e async/await consumindo dados da PokéAPI.',
+        codigo: 'https://github.com/BrunoOliveira16/Pokedex-Api',
+        demo: 'https://pokedex-api-javascript.netlify.app/'
+    },
+    {
+        id: 4,
+        img: imagem04,
+        category: 'HTML/CSS',
+        title: 'Clone HBO Max',
+        description:
+            'Clone do site da HBO Max feito com HTML, CSS e JavaScript. Um projeto para praticar as habilidades de frontend e UI design, assim como designer responsivo',
+        codigo: 'https://github.com/BrunoOliveira16/Clone-HBO-Max',
+        demo: 'https://hbo-max-clone.netlify.app/'
+    },
+    {
+        id: 5,
+        img: imagem05,
+        category: 'React',
+        title: 'Calculadora com React',
+        description:
+            'Um site que usa React para criar uma calculadora simples e funcional. O site usa componentes, estados e hooks para gerenciar a lógica da calculadora.',
+        codigo: 'https://github.com/BrunoOliveira16/calculadora-react',
+        demo: 'https://calculadora-projeto-react.netlify.app/'
+    },
+    {
+        id: 6,
+        img: imagem06,
+        category: 'JavaScript',
+        title: 'Clone Disney+',
+        description:
+            'Este é um site clone do disney+ que utiliza HTML, Sass, JavaScript e gulp para gerenciamento de desenvolvimento, minificação de código e build do projeto.',
+        codigo: 'https://github.com/BrunoOliveira16/disney-clone',
+        demo: 'https://disney-clone-six-eta.vercel.app/'
+    },
+]
+
+export const cv = [
+    {
+        id: 1,
+        title: 'Bacharel em Ciência da Computação',
+        subtitle: 'Faculdade Descomplica',
+        date: '2022 - 2026',
+        description:
+        'Ingressei na faculdade em setembro de 2022, com o objetivo de se especializar ainda mais na área de TI em uma faculdade com foco em desenvolvimento full-stack, desde então tive experiêcia com Java, JavaScript, lógica de programação e algoritimos, analise e requistos de software e banco de dados.',
+        category: 'educacao',
+    },
+    {
+        id: 2,
+        title: 'Desenvolvimento Full-Stack Python',
+        subtitle: 'EBAC',
+        date: '2022 - 2023',
+        description:
+        'Iniciei meus estudos em junho de 2022 neste curso que é focado na linguagem Python para o back-end, finalizei o front-end onde apliquei conhecimentos com javascript, react, bootstrap, jquery, sass e material UI na construção de alguns projetos e no momento venho estudando o back-end com o python.',
+        category: 'educacao',
+    },
+    {
+        id: 3,
+        title: 'Desenvolvedor Front-End',
+        subtitle: 'Freelance',
+        date: '2022 - Até o Momento',
+        description:
+        'Apesar de ter tido algumas experiências com tecnologia anteriores, iniciei de fato meus estudos e transição de carreira em junho de 2022, onde desde então venho realizando alguns projetos front-end com tecnologias como react.',
+        category: 'experiencia',
+    },
+    {
+        id: 4,
+        title: 'Analista de Manutenção',
+        subtitle: 'Ternium Brasil',
+        date: '2017 - 2023',
+        description:
+        'Fui promovido a técnico III como responsável da manutenção do setor de lingotamento continuo, e após fui promovido a analista de manutenção e participei de projetos com foco na analise de dados, para identificação de perfis de perda de manutenção e captura de ganhos em disponibilidade e redução de custos.',
+        category: 'experiencia',
+    },
+    {
+        id: 5,
+        title: 'Engenharia de Produção',
+        subtitle: 'Universidade Cândido Mendes',
+        date: '2013 - 2018',
+        description:
+        'Cursei no período descrito o curso de engenharia no campus de santa cruz e como projeto final apresentei um estudo de caso da aplicação da ferramenta 8D na analise e resolução de problemas de manutenção em equipamentos.',
+        category: 'educacao',
+    },
+    {
+        id: 6,
+        title: 'Técnico de Manutenção Elétrica',
+        subtitle: 'ThyssenKrupp CSA',
+        date: '2010 - 2017',
+        description:
+        'Iniciei como auxiliar e fui promovido até o nível de técnico II como inspetor, fui responsável pela manutenção de equipamentos de um setor inteiro, atuei resolvendo problemas elétricos e de automação de equipamentos siemens como PLC, inversores e motores elétricos.',
+        category: 'experiencia',
+    },
+]
+
---
+++ potfolio/src/components/Home/Home.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import './home.css';
+import profile from '../../assets/profile-img.png'
+import shapeOne from '../../assets/icon-react.png';
+import shapetwo from '../../assets/icon-js.png';
+import shapethree from '../../assets/icon-node.png';
+
+import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
+import CV from '../../assets/Curriculo Bruno Oliveira.pdf'
+
+const Home = () => {
+  return (
+    <section className="home" id="home">
+        <div className="home__wrapper">
+            <div className="home__container container">
+                <p className="home__subtitle text-cs">
+                    Olá, <span>Meu nome é</span>
+                </p>
+
+                <h1 className="home__title text-cs"><span>PABLO</span> GUIMARÃES</h1>
+
+                <p className="home__job">
+                    <span className="text-cs">Eu sou um</span> <b>Desenvolvedor Front-End</b>
+                </p>
+
+                <div className="home__img-wrapper">
+                    <div className="home__banner">
+                        <img src={profile} alt="Minha imagem de perfil" className="home__profile" />
+                    </div>
+
+                    {/*<p className="home__data home__data-one">
+                        <span className="text-lg">
+                            2 <b>+</b>
+                        </span>
+
+                        <span className="text-sm text-cs">
+                            Years of <span>Experience</span>
+                        </span>
+                    </p>
+
+                    <p className="home__data home__data-two">
+                        <span className="text-lg">30</span>
+
+                        <span className="text-sm text-cs">
+                            Completed <span>Projects</span>
+                        </span>
+                    </p>*/}
+
+                    <img src={shapeOne} alt="" className="shape shape__1"/>
+                    <img src={shapetwo} alt="" className="shape shape__2"/>
+                    <img src={shapethree} alt="" className="shape shape__3"/>
+                </div>
+
+                <p className="home__text">
+                    Sou natural do Rio de Janeiro, estudante de Análise e Desenvolvimento de Sistemas, um apaixonado por tecnologia e desenvolvimento Web, atualmente venho evoluindo minhas skills no front-end com React e também venho me especializando em Designer UX/UI.
+                </p>
+
+                <div className="home__socials">
+                    <a href="https://www.linkedin.com/in/bruno-oliveira1608/" className="home__social-link" target="_blank">
+                        <FaLinkedin />
+                    </a>
+
+                    <a href="https://github.com/BrunoOliveira16" className="home__social-link" target="_blank">
+                        <FaGithub />
+                    </a>
+
+                    <a href="https://www.instagram.com/bruno.oliveira.16/" className="home__social-link" target="_blank">
+                        <FaInstagram />
+                    </a>
+                </div>
+
+                <div className="home__btns">
+                    <a download="" href={CV} className="btn text-cs">Dowload CV</a>
+                    {/*<a href="" className="btn text-cs">Dowload CV</a>*/}
+                    <a href="#skills" className="hero__link text-cs">Minhas Skills</a>
+                </div>
+            </div>
+
+            {/* <div className="section__deco deco__left"></div> */}
+        </div>
+    </section>
+  );
+};
+
+export default Home;
