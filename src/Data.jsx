a9b609fa0fb61a04ea0c18eac7f894aad3231d0b
PavloDev <pablogomesdg@gmail.com>
2024-10-25 00:22:01 -0300

    atualiznado projeto

--- src/Data.jsx
+++ src/Data.jsx
@@ -129,57 +129,44 @@ export const projects = [
 export const cv = [
     {
         id: 1,
-        title: 'Bacharel em Ciência da Computação',
-        subtitle: 'Faculdade Descomplica',
-        date: '2022 - 2026',
+        title: 'Análise e Desenvolvimento de Sistemas ',
+        subtitle: 'Bacharel -Universidade Estácio de Sá',
+        date: '2023 - 2025',
         description:
-        'Ingressei na faculdade em setembro de 2022, com o objetivo de se especializar ainda mais na área de TI em uma faculdade com foco em desenvolvimento full-stack, desde então tive experiêcia com Java, JavaScript, lógica de programação e algoritimos, analise e requistos de software e banco de dados.',
+        'Ingressei na faculdade em abril de 2023, atualmente estou cursando o 4º período, com o objetivo de se especializar ainda mais na área de TI em uma faculdade com foco em desenvolvimento full-stack, desde então tive experiêcia com Java, JavaScript, lógica de programação e algoritimos, analise e requistos de software e banco de dados.',
         category: 'educacao',
     },
+    
     {
         id: 2,
-        title: 'Desenvolvimento Full-Stack Python',
-        subtitle: 'EBAC',
-        date: '2022 - 2023',
+        title: 'Assistente Administrativo',
+        subtitle: 'Técnico - SENAI',
+        date: '2020 - 2021',
         description:
-        'Iniciei meus estudos em junho de 2022 neste curso que é focado na linguagem Python para o back-end, finalizei o front-end onde apliquei conhecimentos com javascript, react, bootstrap, jquery, sass e material UI na construção de alguns projetos e no momento venho estudando o back-end com o python.',
+        'Ingressei no curso em um incentivo da empresa Itaúba, onde iniciei o estudo em administração, que foi fundamental posteriormente em outras oportunidades.',
         category: 'educacao',
     },
+
     {
         id: 3,
-        title: 'Desenvolvedor Front-End',
-        subtitle: 'Freelance',
-        date: '2022 - Até o Momento',
+        title: 'Designer',
+        subtitle: 'Pinciara Imóveis',
+        date: '2019 - 2024',
         description:
-        'Apesar de ter tido algumas experiências com tecnologia anteriores, iniciei de fato meus estudos e transição de carreira em junho de 2022, onde desde então venho realizando alguns projetos front-end com tecnologias como react.',
+        'Fui responsável por toda parte visual e web da imobiliária, até que decidi migrar para desenvolvimento.',
         category: 'experiencia',
     },
+
     {
-        id: 4,
-        title: 'Analista de Manutenção',
-        subtitle: 'Ternium Brasil',
-        date: '2017 - 2023',
+        id: 3,
+        title: 'Jovem Aprendiz',
+        subtitle: 'Itaúba Arquitetura e Construções',
+        date: '2020 - 2021',
         description:
-        'Fui promovido a técnico III como responsável da manutenção do setor de lingotamento continuo, e após fui promovido a analista de manutenção e participei de projetos com foco na analise de dados, para identificação de perfis de perda de manutenção e captura de ganhos em disponibilidade e redução de custos.',
+        'Ingressei no curso em um incentivo da empresa Itaúba, sendo o primeiro semestre estudando e o segundo trabalhando.',
         category: 'experiencia',
     },
-    {
-        id: 5,
-        title: 'Engenharia de Produção',
-        subtitle: 'Universidade Cândido Mendes',
-        date: '2013 - 2018',
-        description:
-        'Cursei no período descrito o curso de engenharia no campus de santa cruz e como projeto final apresentei um estudo de caso da aplicação da ferramenta 8D na analise e resolução de problemas de manutenção em equipamentos.',
-        category: 'educacao',
-    },
-    {
-        id: 6,
-        title: 'Técnico de Manutenção Elétrica',
-        subtitle: 'ThyssenKrupp CSA',
-        date: '2010 - 2017',
-        description:
-        'Iniciei como auxiliar e fui promovido até o nível de técnico II como inspetor, fui responsável pela manutenção de equipamentos de um setor inteiro, atuei resolvendo problemas elétricos e de automação de equipamentos siemens como PLC, inversores e motores elétricos.',
-        category: 'experiencia',
-    },
+    
+    
 ]
 
